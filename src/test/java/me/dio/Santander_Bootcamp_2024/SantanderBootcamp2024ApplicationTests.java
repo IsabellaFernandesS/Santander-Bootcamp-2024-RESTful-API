ad91e0d3dc47b7ed3361c5218641240a12b04cb9
Isabella Fernandes <isafernandes.dev@gmail.com>
2024-07-08 17:19:24 -0300

    "Primeiro commit"

---
+++ src/test/java/me/dio/Santander_Bootcamp_2024/SantanderBootcamp2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_Bootcamp_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
